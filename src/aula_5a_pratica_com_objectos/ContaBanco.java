94065da0177f5d178f130aa533c9c17f9f4c7aab
Fenias-Manhenge <53791616+Fenias-Manhenge@users.noreply.github.com>
2020-01-10 14:57:44 +0200

    Conta Banco
    Criacao de uma classe com os seus devidos atributos!

---
+++ src/aula_5a_pratica_com_objectos/ContaBanco.java
@@ -0,0 +1,68 @@
+package aula_5a_pratica_com_objectos;
+
+/**
+ *
+ * @author Fenias
+ */
+
+public class ContaBanco {
+
+    public int numConta;
+    protected String tipo;
+    private String dono;
+    private double saldo;
+    private boolean status;
+
+    public ContaBanco(int numConta, String tipo, String dono, double saldo, boolean status) {
+        this.numConta = numConta;
+        this.tipo = tipo;
+        this.dono = dono;
+        this.saldo = saldo;
+        this.status = status;
+    }
+    
+    public ContaBanco(){
+        
+    }
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
+}
