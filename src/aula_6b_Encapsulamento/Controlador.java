00a06640b387f0099b46d0462f1919adf71263c8
Fenias-Manhenge <53791616+Fenias-Manhenge@users.noreply.github.com>
2020-01-14 22:08:02 +0200

    Codigos!
    Criação de codigos da Classe do controle remoto!

--- src/aula_6b_Encapsulamento/Controlador.java
+++ src/aula_6b_Encapsulamento/Controlador.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aula_6b_Encapsulamento;
 
 /**
@@ -10,5 +5,14 @@ package aula_6b_Encapsulamento;
  * @author Fenias
  */
 public interface Controlador {
-    
+    public abstract void ligar();
+    public abstract void desligar();
+    public abstract void abrirMenu();
+    public abstract void fecharMenu();
+    public abstract void maisVolume();
+    public abstract void menosVolume();
+    public abstract void ligarMudo();
+    public abstract void desligarMudo();
+    public abstract void play();
+    public abstract void pause();
 }
